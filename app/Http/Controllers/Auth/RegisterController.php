8e111b04df7ee51436e840c02e2e6aa2e460bf54
athalla <athallarizk@gmail.com>
2019-05-09 11:13:29 +0700

    firstUpload

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+    //Index
+    public function store(){
+        return "RegisterPages";
+    }
+}
